c974e86f737a9b20a3a7a0476669413e072cfb08
Nicol <nicol@dobartec.net>
2023-09-06 22:59:37 -0400

    Add models Inventory, Material and supplier

--- FleetTechCore/Models/Supply/Inventory.cs
+++ FleetTechCore/Models/Supply/Inventory.cs
@@ -8,6 +8,9 @@ namespace FleetTechCore.Models.Supply
 {
     public class Inventory: AuditableEntity
     {
-
+        public required int     Supplier        { get; set; }
+        public required int     Quantity        { get; set; }
+        public required int     UnitCost        { get; set; }
+        public          int     Material        { get; set; }
     }
 }
---
+++ FleetTechCore/Models/Supply/Material.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetTechCore.Models.Supply
+{
+    public class Material: AuditableEntity
+    {
+        public required string  CodeCategory    { get; set; }
+        public required int     Category        { get; set; }
+        public required string  CodeSubCategory { get; set; }
+        public required int     SubCategory     { get; set; }
+        public required string  CodeMaterial    { get; set; }
+        public required int     Materials       { get; set; }
+        public string           Description     { get; set; }
+        public int              Status          { get; set; }
+    }
+}
